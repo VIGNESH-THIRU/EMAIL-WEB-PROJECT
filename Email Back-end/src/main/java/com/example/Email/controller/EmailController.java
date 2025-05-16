f0767e39cd00dca92333fa2ce29c69e606c31075
VIGNESH-THIRU <vigneshthirumoorthy4@gmail.com>
2025-05-16 12:45:26 +0530

    Update EmailController.java

--- Email Back-end/src/main/java/com/example/Email/controller/EmailController.java
+++ Email Back-end/src/main/java/com/example/Email/controller/EmailController.java
@@ -1,22 +1,4 @@
 package com.example.Email.controller;
-/*Why We Use App Passwords to Send Email in Java (Gmail + SMTP)
-         The Problem:
-        In Java (like Spring Boot), we send email using SMTP, which needs:
-         Your Gmail address
-         Your Gmail password
-        But... Google blocks this for security reasons, especially if:
-        2FA (Two-Factor Authentication) is enabled
-        The app is not using a browser to log in
-        Google’s Solution:
-        To safely allow apps to connect, Google provides something called an App Password.
-         So the Steps Are:
-        Enable 2FA (Two-Factor Authentication) on your Gmail account
-         This protects your account
-        Generate an App Password
-         A special 16-character password like:
-         jnup viyt ataz kcjc
-        Use this App Password in your Java code, instead of your real Gmail password
-         This bypasses 2FA for that app only*/
 import com.example.Email.model.EmailRequest;
 import com.example.Email.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,7 +13,7 @@ public class EmailController {
     private EmailService emailService;
 
     @PostMapping("/send")
-    public String sendEmail(@RequestBody EmailRequest request)//so spring create the object assign the values using lombok
+    public String sendEmail(@RequestBody EmailRequest request)
     {
         try {
             emailService.sendSimpleMail(request.getTo(), request.getSubject(), request.getBody());
